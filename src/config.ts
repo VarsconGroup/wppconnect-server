35a1068a7fcde57649daf9ef481af19eca04521f
Nzeako, Kosisochukwu Anthony <nzeakokosi7@gmail.com>
2024-10-18 00:18:46 +0100

    auto read message true

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export default {
     url: process.env.WEBHOOK,
     autoDownload: true,
     uploadS3: true,
-    readMessage: false,
+    readMessage: true,
     allUnreadOnStart: false,
     listenAcks: true,
     onPresenceChanged: false,
